990bd9289b7b4e843aff091401f088aa12a4dca9
Victor <victormentoymacasaet@gmail.com>
2020-09-01 23:27:20 +0800

    add docker-compose

--- src/engines/google.py
+++ src/engines/google.py
@@ -3,7 +3,7 @@ import requests
 
 
 HEADERS = {
-    'User-Agent': 'Chrome/85.0.4183.83'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36 Edg/85.0.564.41'
 }
 
 
